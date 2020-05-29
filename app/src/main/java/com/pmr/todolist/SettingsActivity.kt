fdc04a9f1daf328f5b37fd78e8b3534cfdddd678
Johan Manuel <johan.manuel@live.fr>
2020-05-29 18:21:37 +0200

    Layout improvements

--- app/src/main/java/com/pmr/todolist/SettingsActivity.kt
+++ app/src/main/java/com/pmr/todolist/SettingsActivity.kt
@@ -1,10 +1,13 @@
 package com.pmr.todolist
 
 import android.app.Activity
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.preference.PreferenceActivity
+import android.view.Menu
+import android.view.MenuItem
 
 class SettingsActivity : PreferenceActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
